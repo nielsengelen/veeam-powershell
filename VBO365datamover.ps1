0f3ff5ad3854917efa4324bcd268c19b2f59132f
nielsengelen <niels@foonet.be>
2019-10-04 14:52:58 +0200

    Version 2.0 release
    Version 2.0 release

--- VBO365datamover.ps1
+++ VBO365datamover.ps1
@@ -1,15 +1,17 @@
 ﻿<# 
 .NAME
-    Veeam Backup for Microsoft Office 365 Data Mover
+    Veeam Backup for Microsoft Office 365 Data Manager
 .SYNOPSIS
-    Leverage this free tool to move user data from 1 repository to another repository.
+    Leverage this free tool to manage Veeam Backup for Microsoft Office 365 data.
 .DESCRIPTION
-    Leverage this free tool to move user data from 1 repository to another repository.
-
+    Free tool to move or remove Veeam Backup for Microsoft Office 365 data.
+.LICENSE
     Released under the MIT license.
 .LINK
     http://www.github.com/nielsengelen
-    http://www.github.com/veeamhub
+    http://www.github.com/VeeamHub
+.VERSION
+    2.0
 #>
 
 Add-Type -AssemblyName System.Windows.Forms 
@@ -17,61 +19,85 @@ Add-Type -AssemblyName System.Windows.Forms
 
 Import-Module "C:\Program Files\Veeam\Backup365\Veeam.Archiver.PowerShell\Veeam.Archiver.PowerShell.psd1"
 
-#region begin GUI{ 
+#region Begin GUI{ 
 $VBO365                          = New-Object system.Windows.Forms.Form
-$VBO365.ClientSize               = '300,275'
-$VBO365.text                     = "VBO365 Data Mover"
-$VBO365.TopMost                  = $false
+$VBO365.ClientSize               = '300,250'
+$VBO365.text                     = "VBO365 Data Manager"
 $VBO365.FormBorderStyle          = 'Fixed3D'
 $VBO365.MaximizeBox              = $false
-$VBO365.MinimizeBox              = $false
 
+#region Labels {
 $lblInfo                         = New-Object system.Windows.Forms.Label
-$lblInfo.text                    = "Use this tool to migrate user data`nto another repository."
+$lblInfo.Text                    = "Use this free community tool to remove data`nor migrate data between repositories."
 $lblInfo.AutoSize                = $true
-$lblInfo.location                = New-Object System.Drawing.Point(15,15)
-$lblInfo.Font                    = 'Microsoft Sans Serif,10'
+$lblInfo.Location                = New-Object System.Drawing.Point(15,10)
+$lblInfo.Font                    = 'Microsoft Sans Serif, 10'
 
 $lblSourceRepo                   = New-Object system.Windows.Forms.Label
-$lblSourceRepo.text              = "Source repository:"
-$lblSourceRepo.AutoSize          = $true
-$lblSourceRepo.location          = New-Object System.Drawing.Point(15,75)
+$lblSourceRepo.Text              = "Source repository:"
+$lblSourceRepo.Location          = New-Object System.Drawing.Point(15,50)
+
+$lblObjectType                   = New-Object system.Windows.Forms.Label
+$lblObjectType.Text              = "Select type:"
+$lblObjectType.Location          = New-Object System.Drawing.Point(15,75)
+
+$lblObject                       = New-Object system.Windows.Forms.Label
+$lblObject.Text                  = "Select object:"
+$lblObject.Location              = New-Object System.Drawing.Point(15,100)
 
 $lblTargetRepo                   = New-Object system.Windows.Forms.Label
-$lblTargetRepo.text              = "Target repository:"
-$lblTargetRepo.AutoSize          = $true
-$lblTargetRepo.location          = New-Object System.Drawing.Point(15,125)
-
-$lblUser                         = New-Object system.Windows.Forms.Label
-$lblUser.text                    = "Select user:"
-$lblUser.AutoSize                = $true
-$lblUser.location                = New-Object System.Drawing.Point(15,100)
-
-$cmbSourceRepo                   = New-Object system.Windows.Forms.ComboBox
-$cmbSourceRepo.width             = 150
-$cmbSourceRepo.location          = New-Object System.Drawing.Point(135,75)
-
-$cmbTargetRepo                   = New-Object system.Windows.Forms.ComboBox
-$cmbTargetRepo.width             = 150
-$cmbTargetRepo.location          = New-Object System.Drawing.Point(135,125)
-
-$cmbUsers                        = New-Object system.Windows.Forms.ComboBox
-$cmbUsers.width                  = 150
-$cmbUsers.location               = New-Object System.Drawing.Point(135,100)
-
-$btnSubmit                       = New-Object system.Windows.Forms.Button
-$btnSubmit.text                  = "Migrate"
-$btnSubmit.width                 = 80
-$btnSubmit.height                = 30
-$btnSubmit.location              = New-Object System.Drawing.Point(110,155)
+$lblTargetRepo.Text              = "Target repository:"
+$lblTargetRepo.Location          = New-Object System.Drawing.Point(15,125)
 
 $lblDisclaimer                   = New-Object system.Windows.Forms.Label
-$lblDisclaimer.text              = "Copyright (c) 2018 VeeamHub`n`nDistributed under MIT license."
+$lblDisclaimer.Text              = [char]0x00A9 + " 2019 VeeamHub`n`nDistributed under MIT license."
 $lblDisclaimer.AutoSize          = $true
-$lblDisclaimer.location          = New-Object System.Drawing.Point(35,200)
-$lblDisclaimer.Font              = 'Microsoft Sans Serif,10'
+$lblDisclaimer.Location          = New-Object System.Drawing.Point(55,200)
+$lblDisclaimer.Font              = 'Microsoft Sans Serif, 10'
+#endregion Labels }
 
-$VBO365.Controls.AddRange(@($lblInfo,$lblSourceRepo,$lblTargetRepo,$lblUser,$cmbSourceRepo,$cmbTargetRepo,$cmbUsers,$btnSubmit, $lblDisclaimer))
+#region Dropdown list {
+$dropDownBoxType                 = New-Object System.Windows.Forms.ComboBox
+$dropDownBoxType.Location        = New-Object System.Drawing.Size(135,75)
+$dropDownBoxType.Size            = New-Object System.Drawing.Size(150,20) 
+$dropDownBoxType.DropDownStyle   = 'DropDownList'
+
+[void]$dropDownBoxType.Items.Add("User")
+[void]$dropDownBoxType.Items.Add("Group")
+[void]$dropDownBoxType.Items.Add("Site")
+
+$dropDownBoxType.SelectedItem    = $dropDownBoxType.Items[0]
+#endregion Dropdown list }
+
+#region Comboboxes
+$cmbSourceRepo                   = New-Object system.Windows.Forms.ComboBox
+$cmbSourceRepo.Width             = 150
+$cmbSourceRepo.Location          = New-Object System.Drawing.Point(135,50)
+
+$cmbObject                       = New-Object system.Windows.Forms.ComboBox
+$cmbObject.Width                 = 150
+$cmbObject.Location              = New-Object System.Drawing.Point(135,100)
+
+$cmbTargetRepo                   = New-Object system.Windows.Forms.ComboBox
+$cmbTargetRepo.Width             = 150
+$cmbTargetRepo.Location          = New-Object System.Drawing.Point(135,125)
+#endregion Comboboxes }
+
+#region Buttons {
+$btnDelete                       = New-Object system.Windows.Forms.Button
+$btnDelete.Text                  = "Delete"
+$btnDelete.Width                 = 80
+$btnDelete.Height                = 30
+$btnDelete.Location              = New-Object System.Drawing.Point(50,160)
+
+$btnMigrate                      = New-Object system.Windows.Forms.Button
+$btnMigrate.Text                 = "Migrate"
+$btnMigrate.Width                = 80
+$btnMigrate.Height               = 30
+$btnMigrate.Location             = New-Object System.Drawing.Point(170,160)
+#endregion Buttons }
+
+$VBO365.Controls.AddRange(@($lblInfo, $lblObject, $lblObjectType, $lblSourceRepo, $lblTargetRepo, $lblDisclaimer, $dropDownBoxType, $cmbObject, $cmbSourceRepo, $cmbTargetRepo, $btnDelete, $btnMigrate))
 
 #region gui events {
 $reposList = Get-VBORepository
@@ -82,20 +108,77 @@ foreach ($repos in $reposList) {
 }
 
 $cmbSourceRepo.Add_SelectedIndexChanged({
-  $cmbUsers.Items.Clear()
-  $cmbSourceRepo.Text = ""
+  $cmbObject.Items.Clear()
+  $cmbObject.Text = ""
   $cmbTargetRepo.Text = ""
-  $cmbUsers.Text = ""
 
   $repo = Get-VBORepository -Name $cmbSourceRepo.SelectedItem
-  $usersList = Get-VBOEntityData -Type User -Repository $repo
+  $objectsList = Get-VBOEntityData -Type $dropDownBoxType.SelectedItem -Repository $repo
 
-  foreach ($users in $usersList) {
-   [void] $cmbUsers.Items.Add($users.DisplayName)
+  foreach ($objects in $objectsList) {
+   [void] $cmbObject.Items.Add($objects.DisplayName)
   }
 })
 
-$btnSubmit.Add_Click({
+$dropDownBoxType.Add_SelectedIndexChanged({
+  $cmbObject.Items.Clear()
+  $cmbObject.Text = ""
+  $cmbTargetRepo.Text = ""
+
+  $repo = Get-VBORepository -Name $cmbSourceRepo.SelectedItem
+  $objectsList = Get-VBOEntityData -Type $dropDownBoxType.SelectedItem -Repository $repo
+
+  foreach ($objects in $objectsList) {
+   [void] $cmbObject.Items.Add($objects.DisplayName)
+  }
+})
+
+$btnDelete.Add_Click({
+  $sourceRepo = $cmbSourceRepo.SelectedItem
+
+  if (!$sourceRepo) {
+    [System.Windows.Forms.MessageBox]::Show("Please select a source repository.", "Error", 0, 48)
+  } else {
+    $object = $cmbObject.SelectedItem
+
+    if (!$object) {
+      [System.Windows.Forms.MessageBox]::Show("No object selected.", "Error" , 0, 48)
+    } else {
+      $repo = Get-VBORepository -Name $sourceRepo
+      $objectdata = Get-VBOEntityData -Type $dropDownBoxType.SelectedItem -Repository $repo -Name $object
+      $type = $dropDownBoxType.SelectedItem
+
+      if ($type -eq 'User') {
+         try {
+           Remove-VBOEntityData -Repository $repo -User $objectdata
+         } catch {
+           [System.Windows.Forms.MessageBox]::Show("Failed to remove data.", "Error" , 0, 48)
+         }
+      } elseif ($type -eq 'Group') {
+         try {
+           Remove-VBOEntityData -Repository $repo -Group $objectdata
+         } catch {
+           [System.Windows.Forms.MessageBox]::Show("Failed to remove data.", "Error" , 0, 48)
+         }
+      } elseif ($type -eq 'Site') {
+         try {
+           Remove-VBOEntityData -Repository $repo -Site $objectdata
+         } catch {
+           [System.Windows.Forms.MessageBox]::Show("Failed to remove data.", "Error" , 0, 48)
+         }
+      }
+
+      $cmbObject.Items.Clear()
+      $cmbObject.Text = ""
+      $cmbSourceRepo.Text = ""
+      $cmbTargetRepo.Text = ""
+
+      [System.Windows.Forms.MessageBox]::Show("Data has been removed.", "Success", 0, 64)
+    }
+  }
+})
+
+$btnMigrate.Add_Click({
   $sourceRepo = $cmbSourceRepo.SelectedItem
   $targetRepo = $cmbTargetRepo.SelectedItem
 
@@ -107,29 +190,46 @@ $btnSubmit.Add_Click({
     if ($sourceRepo -eq $targetRepo) {
       [System.Windows.Forms.MessageBox]::Show("Source and target repository are the same.", "Error" , 0, 48)
     } else {
-      $user = $cmbUsers.SelectedItem
+      $object = $cmbObject.SelectedItem
 
-      if (!$user) {
-        [System.Windows.Forms.MessageBox]::Show("No user selected.", "Error" , 0, 48)
+      if (!$object) {
+        [System.Windows.Forms.MessageBox]::Show("No object selected.", "Error" , 0, 48)
       } else {
         $source = Get-VBORepository -Name $sourceRepo
         $target = Get-VBORepository -Name $targetRepo
-        $userdata = Get-VBOEntityData -Type User -Repository $source -Name $user
+        $objectdata = Get-VBOEntityData -Type $dropDownBoxType.SelectedItem -Repository $source -Name $object
+        $type = $dropDownBoxType.SelectedItem
 
-        Move-VBOEntityData -From $source -To $target -User $userdata -Mailbox -ArchiveMailbox -OneDrive -Sites
+        if ($type -eq 'User') {
+          try {
+            Move-VBOEntityData -From $source -To $target -User $objectdata
+          } catch {
+           [System.Windows.Forms.MessageBox]::Show("Failed to move data.", "Error" , 0, 48)
+          }
+        } elseif ($type -eq 'Group') {
+          try {
+            Move-VBOEntityData -From $source -To $target -Group $objectdata
+          } catch {
+           [System.Windows.Forms.MessageBox]::Show("Failed to move data.", "Error" , 0, 48)
+          }
+        } elseif ($type -eq 'Site') {
+          try {
+            Move-VBOEntityData -From $source -To $target -Site $objectdata
+          } catch {
+           [System.Windows.Forms.MessageBox]::Show("Failed to move data.", "Error" , 0, 48)
+          }
+        }
 
-        $cmbTargetRepo.Items.clear()
-        $cmbUsers.Items.Clear()
+        $cmbObject.Items.Clear()
+        $cmbObject.Text = ""
+        $cmbSourceRepo.Text = ""
         $cmbTargetRepo.Text = ""
-        $cmbUsers.Text = ""
 
         [System.Windows.Forms.MessageBox]::Show("Data has been moved.", "Success", 0, 64)
       }
     }
   }
 })
-#endregion events }
-#endregion GUI }
-
+#endregion End GUI }
 
 [void]$VBO365.ShowDialog()
